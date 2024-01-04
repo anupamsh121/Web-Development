cd3c58ea219fb00da78ae5bdd97a4ccb2e8961f3
anupam022 <anupamsharma237@gmail.com>
2024-01-04 18:37:28 +0530

    Written first webpage

---
+++ Frontend/object.js
@@ -0,0 +1,38 @@
+let person = {              //this keyword is used to declare keyword
+    firstname: "Anupam",
+    lastname: "Sharma",
+    age:22,
+    country:"India"
+    
+
+}
+console.log(person)
+console.log(typeof person)
+console.log(person.firstname,person.lastname)
+console.log(person['age'])
+console.log(JSON.stringify(person))        ///Stringify  --> check for mcq
+
+let myObj = console.log(JSON.parse('{"username": "anonym","password":"12345","age":"22"}'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
